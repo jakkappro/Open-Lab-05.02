01cd4a46e84a623d480a7982f228b4b6975641de
jakub kapitulcin <kapitulcinjakub1@gmail.com>
2020-10-03 09:28:06 +0200

    I addet function to remove given character from last position and function to remove all unneseesery ? or !

--- Open-Lab-05.02/StringTools.cs
+++ Open-Lab-05.02/StringTools.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace Open_Lab_05._02
 {
@@ -6,7 +7,35 @@ namespace Open_Lab_05._02
     {
         public string NoYelling(string sentence)
         {
-            throw new NotImplementedException();
+            string ret = "";
+
+            if(sentence.Contains("!"))
+            {
+                removeChar(sentence, '!', out ret);
+                return ret + "!";
+            }
+
+            if(sentence.Contains("?"))
+            {
+                removeChar(sentence, '?', out ret);
+                return ret + "?";
+            }
+
+            return sentence;
+        }
+
+        void removeChar(String str, char remove, out string stri)
+        {
+            if(str[str.Length - 1] == remove)
+            {
+                
+                removeChar(str.Substring(0, str.Length - 1), remove, out stri);
+            }
+            else
+            {
+                stri = str.Substring(0, str.Length); 
+            }
+            
         }
     }
 }
